5d06a6aa277e1b50e76e3809aec814dcc6580234
Fixerin <jan.malinski94@gmail.com>
2019-01-28 23:11:47 +0100

    Dodanie

---
+++ Lista_owocow.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Projekt_na_studia
+{
+    class Program
+    {
+        static void wyswietlTablice(List<string> owoce)
+        {
+            Console.WriteLine("Twoje owoce:");
+            Console.WriteLine("------------");
+            for (int i = 0; i < owoce.Count; i++)
+            {
+                Console.WriteLine(owoce[i]);
+
+            }
+            Console.WriteLine(); // jako przejscie do nowej linii
+        }
+
+        static void dodajOwoc(List<string> owoce)
+        {
+            string nowyOwoc;
+            Console.Write("Podaj nazwe owocu do dodania: ");
+            nowyOwoc = Console.ReadLine();
+            owoce.Add(nowyOwoc);
+
+            Console.WriteLine("\nWlasnie dodales {0} do tablicy owocow.\n", nowyOwoc);
+        }
+
+        static void edytujOwoc(List<string> owoce)
+        {
+            string szukanyOwoc;
+            string nowyOwoc;
+            Console.Write("Podaj nazwe owocu do edycji: ");
+            szukanyOwoc = Console.ReadLine();
+            if (owoce.Contains(szukanyOwoc))
+            {
+                Console.Write("\n Podaj nazwe nowego owocu: ");
+                nowyOwoc = Console.ReadLine();
+
+                owoce[owoce.FindIndex(ind => ind.Equals(szukanyOwoc))] = nowyOwoc;
+
+                Console.WriteLine("Zmieniono {0} na {1}.", szukanyOwoc, nowyOwoc);
+
+            }
+            else
+            {
+                Console.WriteLine("Nie znaleziono takiego owocu w tablicy.");
+            }
+            
+        }
+
+        static void usunOwoc(List<string> owoce)
+        {
+            string doUsuniecia;
+            Console.Write("Jaki owoc chcesz usunac z tablicy? ");
+            doUsuniecia = Console.ReadLine();
+
+            if (owoce.Contains(doUsuniecia))
+            {
+                owoce.Remove(doUsuniecia);
+                Console.WriteLine("Usunieto {0} z tablicy.");
+            }
+            else
+            {
+                Console.WriteLine("Nie znaleziono takiego owocu w tablicy.");
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            var owoce = new List<string>();
+            int wybor;
+            string wyborStr;
+
+            owoce.Add("Banan");
+
+            Console.WriteLine(owoce[0] + "\n");
+
+                do {
+                    Console.WriteLine("MENU:");
+                    Console.WriteLine("[1] Wyswietl tablice owocow.");
+                    Console.WriteLine("[2] Dodaj owoc do tablicy.");
+                    Console.WriteLine("[3] Edytuj dany owoc.");
+                    Console.WriteLine("[4] Usun owoc z tablicy.");
+                    Console.WriteLine("[0] Zamknij aplikacje.\n");
+
+                
+                    Console.Write("Wybierz opcje: ");
+                    wyborStr = Console.ReadLine();
+                    try
+                    {
+                        wybor = Convert.ToInt32(wyborStr);
+                    }
+                    catch (FormatException) { Console.WriteLine("Bledne dane wejsciowe!"); Console.ReadKey(); return;}
+
+
+                    wybor = Convert.ToInt32(wyborStr);
+
+                    Console.Write("\n");
+
+
+
+                    switch (wybor)
+                    {
+                        case 1:
+                            wyswietlTablice(owoce);
+                            break;
+
+                        case 2:
+                            dodajOwoc(owoce);
+                            break;
+
+                        case 3:
+                            edytujOwoc(owoce);
+                            break;
+
+                        case 4:
+                            usunOwoc(owoce);
+                            break;
+
+                        case 0:
+                            Console.WriteLine("Dziekuje za skorzystanie z aplikacji!");
+                            break;
+
+                        default:
+                            Console.WriteLine("Nie ma takiej opcji.\n");
+                            break;
+                    }
+                
+
+                } while (wybor != 0);
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Proste_funkcje_matematyczne.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Projekt2
+{
+    class Program
+    {
+
+        static string converting(string number, int fromBase, int toBase)
+        {
+
+            string result = Convert.ToString(Convert.ToInt32(number, fromBase), toBase);
+            return result;
+        }
+
+        static int[] wypelnienie(int n)
+        {
+
+            int[] tabLiczb = new int[n];
+
+            Console.WriteLine("\nWypisz te liczby: ");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Wpisz liczbe nr {0}: ", i + 1);
+                tabLiczb[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            return tabLiczb;
+        }
+
+        static void pierwiastkowanie()
+        {
+            double liczba, stopien, wynik;
+            Console.Write("\nPodaj liczbe do pierwiastkowania: ");
+            liczba = Convert.ToDouble(Console.ReadLine());
+            Console.Write("\nPodaj stopien pierwiastka: ");
+            stopien = Convert.ToDouble(Console.ReadLine());
+
+            wynik = Math.Pow(liczba, 1.0 / stopien);
+
+            Console.WriteLine("\nPierwiastek {0} stopnia z {1} wynosi {2}\n",stopien, liczba, wynik);
+
+            Console.ReadKey();
+
+        }
+
+        static void potegowanie()
+        {
+            double liczba, stopien, wynik;
+            Console.Write("\nPodaj liczbe do potegowania: ");
+            liczba = Convert.ToDouble(Console.ReadLine());
+            Console.Write("\nPodaj stopien potegi: ");
+            stopien = Convert.ToDouble(Console.ReadLine());
+
+            wynik = Math.Pow(liczba, stopien);
+
+            Console.WriteLine("\n{0} podniesione do {1} postegi wynosi {2}", liczba, stopien, wynik);
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+
+        static void systemLiczby()
+        {
+            int baseInt, toInt;
+            string wynikStr, liczbaStr;
+
+            Console.Write("\nZ jakiego systemu chcesz przekonwertowac liczbe? (podaj liczbe systemu) ..");
+            baseInt = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("\nNa jaki system przekonwertować? (podaj liczbe systemu) ..");
+            toInt = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("\nPodaj liczbe do konwersji: ");
+            //liczba = Convert.ToInt32(Console.ReadLine());
+            liczbaStr = Console.ReadLine();
+
+            wynikStr = converting(liczbaStr, baseInt, toInt);
+            Console.WriteLine("\n{0} w systemie {1} przekonwertowana na system {2} to {3}", liczbaStr, baseInt, toInt, wynikStr);
+
+            Console.ReadKey();
+
+        }
+
+        
+
+
+
+        static void minMax()
+        {
+            int n;
+            
+            Console.Write("\nZ posrod ilu liczb chcesz wyszukac min/max? ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            int[] tabLiczb = wypelnienie(n);
+
+            int min = tabLiczb[0];
+            int max = tabLiczb[0];
+
+            for (int i = 1; i < n; i++)
+            {
+                if (min > tabLiczb[i]) min = tabLiczb[i];
+                if (max < tabLiczb[i]) max = tabLiczb[i];
+            }
+
+            Console.WriteLine("\nNajmniejsza liczba to: {0}", min);
+            Console.WriteLine("Najwieksza liczba to: {0}", max);
+
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+
+        static void szeregowanie()
+        {
+            int n;
+
+            Console.Write("\nWpisz ilosc licz do szeregowania? ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            int[] tabLiczb = wypelnienie(n);
+
+            Array.Sort(tabLiczb);
+            // write array
+            Console.WriteLine("\nPosortowane liczby od najmniejszej do najwiekszej:");
+            foreach (int i in tabLiczb) Console.Write(i + " ");  
+
+            Array.Reverse(tabLiczb);
+
+            Console.WriteLine("\nPosortowane liczby od najwiekszej do najmniejszej:");
+            foreach (int i in tabLiczb) Console.Write(i + " ");
+
+            Console.WriteLine("\n\n");
+
+            Console.ReadKey();
+
+        }
+
+        
+
+        static void zaokraglanie()
+        {
+            int n;
+            double liczba, wynik;
+            string liczbaStr;
+
+            Console.Write("\nDo ilu miejsc po przecinku chcesz zaokraglic? ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("\nJaka liczbe chcesz zaokraglic? ");
+            liczbaStr = Console.ReadLine();
+
+            liczba = double.Parse(liczbaStr);
+
+            wynik = Math.Round(liczba, n);
+
+            Console.WriteLine("{0} zaokraglona do {1} miejsc po przecinku wynosi {2}", liczba, n, wynik);
+            Console.WriteLine();
+
+            Console.ReadKey();
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            int wybor;
+
+            do
+            {
+                Console.WriteLine("Menu: \n");
+                Console.WriteLine("[1] Pierwiastkowanie");
+                Console.WriteLine("[2] Potegowanie");
+                Console.WriteLine("[3] Zmiana na zapis liczby w różnych systemach");
+                Console.WriteLine("[4] Znajdowanie min/max z podanych liczb");
+                Console.WriteLine("[5] Szeregowanie liczb wg wielkosci");
+                Console.WriteLine("[6] Zaokraglanie do zadanej ilosci miejsc po przecinku");
+                Console.WriteLine("[0] Zamknij aplikacje\n");
+
+                Console.Write("Wybierz numer opcji: ");
+                wybor = Convert.ToInt32(Console.ReadLine());
+
+                switch (wybor)
+                {
+                    case 1:
+                        pierwiastkowanie();
+                        break;
+
+                    case 2:
+                        potegowanie();
+                        break;
+
+                    case 3:
+                        systemLiczby();
+                        break;
+
+                    case 4:
+                        minMax();
+                        break;
+
+                    case 5:
+                        szeregowanie();
+                        break;
+
+                    case 6:
+                        zaokraglanie();
+                        break;
+
+                    case 0:
+                        Console.WriteLine("\nDziekuje za skorzystanie z aplikacji!");
+                        break;
+
+                    default:
+                        Console.WriteLine("Wprowadzono bledny numer opcji!");
+                        break;
+                }
+
+
+            } while (wybor != 0);
+
+            Console.ReadKey();
+
+        }
+    }
+}
